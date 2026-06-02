6840b1c858615ba0cc317ead1b592ef9ef944f27
hbphucc <hoangbaophuc234@gmail.com>
2026-06-02 15:53:59 +0700

    Demo Docker

--- Frontend/lib/axios.ts
+++ Frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "https://localhost:5001/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
